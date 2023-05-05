39622bcebc8a84459660919169fb9e8375162b7c
alaabadah4 <alaabadah4@gmail.com>
2023-05-05 14:41:27 +0300

    hw2

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import Screens from "./navigation";
+
+function App() {
+  
+
+  return (
+    <View style={{ flex: 1 }}>
+            <Screens />
+    </View>
+  );
+}
+
+
+
+export default App;
